1ae2c47f3f0341bc621f46d649a80193a57af30a
Jake Kopulsky <jakekopulsky@gmail.com>
2018-06-24 23:20:11 -0700

    client connect, set username, enter room

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-const express = require('express');
-const app = express();
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', (req, res) => res.send('Hello World'));
+// Listen on port 3000
+http.listen(3000);
 
-app.listen(3000, () => console.log('Terminal chat listening on post 3000'));
+// app.get('/', function (req, res) {
+//   res.sendfile(__dirname + '/index.html');
+// });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  // Default username
+  socket.username = 'Anonymous';
+  socket.room = 'lobby'
+
+  socket.emit('welcome', { message: 'Welcome to terminal chat' });
+
+  socket.on('change_username', ({ username }) => {
+    socket.username = username;
+  });
+
+  socket.on('change_room', ({ room }) => {
+    socket.room = room;
+  });
+
+  socket.on('disconnect', () => console.log('user disconnected'));
+});
--- interface.js
+++ interface.js
@@ -1,11 +1,79 @@
 const readline = require('readline');
+const socketio = require('socket.io-client');
+const color = require('ansi-colors');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+var socket = socketio.connect('http://localhost:3000');
 
-rl.question('Enter your username: ', (username) => {
-  console.log(`Username is ${username}`);
-  rl.close();
-});
+// let username = '';
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+// Use this function to log to console
+const console_out = (msg) => {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  console.log(msg);
+  rl.prompt(true);
+};
+
+// Set username
+// const setUsername = () => {
+//   rl.question('Enter your username: ', (username) => {
+//     let msg = `${username} has connected!`;
+//     socket.emit('send', { type: 'notice', message: msg });
+//     rl.prompt(true);
+//   });
+// }
+
+// rl.on('line', (line) => {
+//   socket.emit('send', { type: 'chat', line: line, username: username});
+//   rl.prompt(true);
+// });
+
+// socket.on('message', (data) => {
+//     console.log(data);
+//     // if (data.type === 'chat') {
+//     //   console_out(`${data.username}: ${data.message}`);
+//     // }
+// });
+
+// socket.on('welcome', ({ message }) => {
+//   console_out('/////////////////');
+//   console_out(message);
+//   console_out('/////////////////');
+//   // setUsername();
+//   // socket.emit('my other event', { my: 'data' });
+// });
+
+const Client = function() {
+  this.username = "Anonymous";
+  this.room = "lobby";
+}
+
+Client.prototype.connect = function() {
+  socket.on('welcome', (data) => {
+    console_out('/////////////////');
+    console_out(data.message);
+    console_out('/////////////////');
+    this.setUsername();
+  });
+}
+
+Client.prototype.setUsername = function() {
+  rl.question('Enter your username: ', (username) => {
+    socket.emit('change_username', { username: username });
+    this.username = username;
+    this.setRoom();
+  });
+}
+
+Client.prototype.setRoom = function() {
+  rl.question('Room: ', (room) => {
+    socket.emit('change_room', { room: room });
+    this.room = room;
+    rl.prompt(true);
+  });
+}
+
+let newConnection = new Client();
+newConnection.connect();
